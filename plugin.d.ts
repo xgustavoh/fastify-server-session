1ca9a3849906dc03cc2a0b33d4cbbe4e1f75368a
Gustavo Minari <gustavo_minari@hotmail.com.br>
2020-03-12 18:44:56 -0300

    feat: 🎸 Session in query and Create TS Define

---
+++ plugin.d.ts
@@ -0,0 +1,64 @@
+/// <reference types="node" />
+import * as fastify from "fastify";
+import { IncomingMessage } from "http";
+import { Http2ServerRequest } from "http2";
+
+type HttpRequest = IncomingMessage | Http2ServerRequest;
+type HttpResponse = ServerResponse | Http2ServerResponse;
+
+declare module "fastify" {
+  interface FastifyRequest<
+    HttpRequest,
+    Query = fastify.DefaultQuery,
+    Params = fastify.DefaultParams,
+    Headers = fastify.DefaultHeaders,
+    Body = any
+  > {
+    /** Allows to access or modify the session data. */
+    session: Session;
+  }
+
+  interface Session extends Record<string, any> {
+    id: string;
+    token: string;
+  }
+}
+
+declare function FastifyServerSessionPlugin(): void;
+
+declare interface FastifyServerSessionPlugin<
+  HttpServer,
+  HttpRequest,
+  HttpResponse
+>
+  extends fastify.Plugin<
+    HttpServer,
+    HttpRequest,
+    HttpResponse,
+    FastifyServerSessionPlugin.Options
+  > {}
+
+declare namespace FastifyServerSessionPlugin {
+  interface Options {
+    cookie?: CookieOptions;
+    secretKey: string;
+    sessionCookieName?: string;
+    sessionMaxAge?: string;
+  }
+
+  interface CookieOptions {
+    domain?: string;
+    expires?: number;
+    httpOnly?: boolean;
+    path?: string;
+    sameSite?: boolean;
+  }
+}
+
+declare var FastifyServerSessionPlugin: FastifyServerSessionPlugin<
+  any,
+  any,
+  any
+>;
+
+export = fastifyServerSession;
--- plugin.js
+++ plugin.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
-const { sign, unsign } = require('cookie-signature')
-const uidgen = require('uid-safe')
-const merge = require('merge-options')
-const MAX_AGE = 1800000 // 30 minutes
+const fp = require("fastify-plugin");
+const { sign, unsign } = require("cookie-signature");
+const uidgen = require("uid-safe");
+const merge = require("merge-options");
+const MAX_AGE = 1800000; // 30 minutes
 const defaultOptions = {
   cookie: {
     domain: undefined,
@@ -14,112 +14,138 @@ const defaultOptions = {
     sameSite: true
   },
   secretKey: undefined,
-  sessionCookieName: 'sessionid',
+  sessionCookieName: "sessionid",
   sessionMaxAge: MAX_AGE
-}
-const getSession = require('./lib/session')
-const { symbols: syms } = getSession
+};
+const getSession = require("./lib/session");
+const { symbols: syms } = getSession;
 
-function plugin (fastify, options, pluginRegistrationDone) {
-  const _options = Function.prototype.isPrototypeOf(options) ? {} : options
-  const opts = merge({}, defaultOptions, _options)
+function plugin(fastify, options, pluginRegistrationDone) {
+  const _options = Function.prototype.isPrototypeOf(options) ? {} : options;
+  const opts = merge({}, defaultOptions, _options);
+
   if (!opts.secretKey) {
-    return pluginRegistrationDone(Error('must supply secretKey'))
+    return pluginRegistrationDone(Error("must supply secretKey"));
   }
   // https://security.stackexchange.com/a/96176/38214
   if (opts.secretKey.length < 32) {
     return pluginRegistrationDone(
-      Error('secretKey must be at least 32 characters')
-    )
+      Error("secretKey must be at least 32 characters")
+    );
   }
   if (opts.cookie.expires && !Number.isInteger(opts.cookie.expires)) {
     return pluginRegistrationDone(
-      Error('cookie expires time must be a value in milliseconds')
-    )
+      Error("cookie expires time must be a value in milliseconds")
+    );
   }
 
-  fastify.decorateRequest('session', getSession())
-  fastify.addHook('onRequest', function (req, reply, hookFinished) {
-    if (!req.cookies[opts.sessionCookieName]) {
-      req.session = getSession()
-      return hookFinished()
-    }
-
-    const sessionId = unsign(
-      req.cookies[opts.sessionCookieName],
-      opts.secretKey
-    )
-    req.log.trace('sessionId: %s', sessionId)
-    if (sessionId === false) {
-      req.log.warn('session id signature mismatch, starting new session')
-      req.session = getSession()
-      return hookFinished()
-    }
-
-    this.cache.get(sessionId, (err, cached) => {
-      if (err) {
-        req.log.trace('could not retrieve session data')
-        return hookFinished(err)
-      }
-      if (!cached) {
-        req.log.trace('session data missing (new/expired)')
-        req.session = getSession()
-        return hookFinished()
-      }
-      req.session = getSession(cached.item)
-      req.log.trace('session restored: %j', req.session)
-      hookFinished()
-    })
-  })
-
-  fastify.addHook('onSend', function (req, reply, payload, hookFinished) {
-    if (req.session[syms.kSessionModified] === false) {
-      return hookFinished()
-    }
-
+  function getSessionID(req, done) {
     if (req.cookies[opts.sessionCookieName]) {
-      const id = unsign(req.cookies[opts.sessionCookieName], opts.secretKey)
-      return storeSession.call(this, null, id)
+      const sessionId = unsign(
+        req.cookies[opts.sessionCookieName],
+        opts.secretKey
+      );
+      req.log.trace("sessionId: %s", sessionId);
+
+      if (sessionId) {
+        return done(null, {
+          id: sessionId,
+          enc: req.cookies[opts.sessionCookieName]
+        });
+      }
     }
 
-    uidgen(18, storeSession.bind(this))
+    if (req.query[opts.sessionCookieName]) {
+      const sessionId = unsign(
+        req.query[opts.sessionCookieName],
+        opts.secretKey
+      );
+      req.log.trace("sessionId: %s", sessionId);
 
-    function storeSession (err, sessionId) {
+      if (sessionId) {
+        return done(null, {
+          id: sessionId,
+          enc: req.query[opts.sessionCookieName]
+        });
+      }
+    }
+
+    uidgen(18, storeSession.bind(this));
+    function storeSession(err, sessionId) {
       if (err) {
-        req.log.trace('could not store session with invalid id')
-        return hookFinished(err)
+        req.log.trace("could not store session with invalid id");
+        done(err);
+      } else if (!sessionId) {
+        req.log.trace("could not store session with missing id");
+        done(Error("missing session id"));
+      } else {
+        done(null, { id: sessionId, enc: sign(sessionId, opts.secretKey) });
       }
-
-      if (!sessionId) {
-        req.log.trace('could not store session with missing id')
-        return hookFinished(Error('missing session id'))
-      }
-
-      this.cache.set(sessionId, req.session, opts.sessionMaxAge, err => {
-        if (err) {
-          req.log.trace('error saving session: %s', err.message)
-          return hookFinished(err)
-        }
-        const cookieExiresMs = opts.cookie && opts.cookie.expires
-        const cookieOpts = merge({}, opts.cookie, {
-          expires: !cookieExiresMs
-            ? undefined
-            : new Date(Date.now() + cookieExiresMs)
-        })
-        const signedId = sign(sessionId, opts.secretKey)
-        reply.setCookie(opts.sessionCookieName, signedId, cookieOpts)
-        hookFinished()
-      })
     }
-  })
+  }
 
-  pluginRegistrationDone()
+  fastify.decorateRequest("session", getSession());
+  fastify.addHook("onRequest", function(req, reply, hookFinished) {
+    getSessionID(req, function(err, session) {
+      if (err || !session) {
+        req.session = getSession();
+      } else {
+        this.cache.get(session.id, (err, cached) => {
+          if (err) {
+            req.log.trace("could not retrieve session data");
+            hookFinished(err);
+          } else if (!cached) {
+            req.log.trace("session data missing (new/expired)");
+            req.session = getSession(session);
+            hookFinished();
+          } else {
+            req.session = getSession(session, cached.item);
+            req.log.trace("session restored: %j", req.session);
+            hookFinished();
+          }
+        });
+      }
+    });
+  });
+
+  fastify.addHook("onSend", function(req, reply, payload, hookFinished) {
+    if (req.session[syms.kSessionModified] === false) {
+      hookFinished();
+    } else {
+      this.cache.set(
+        req.session[syms.kSessionID],
+        req.session,
+        opts.sessionMaxAge,
+        err => {
+          if (err) {
+            req.log.trace("error saving session: %s", err.message);
+            hookFinished(err);
+          } else {
+            const cookieExiresMs = opts.cookie && opts.cookie.expires;
+            const cookieOpts = merge({}, opts.cookie, {
+              expires: !cookieExiresMs
+                ? undefined
+                : new Date(Date.now() + cookieExiresMs)
+            });
+            reply.setCookie(
+              opts.sessionCookieName,
+              req.session[syms.kSessionToken],
+              cookieOpts
+            );
+            hookFinished();
+          }
+        }
+      );
+    }
+  });
+
+  pluginRegistrationDone();
 }
 
 module.exports = fp(plugin, {
-  fastify: '^2.0.0',
-  dependencies: ['fastify-cookie'],
+  fastify: "^2.0.0",
+  dependencies: ["fastify-cookie"],
   decorators: {
-    fastify: ['cache']
+    fastify: ["cache"]
   }
-})
+});
